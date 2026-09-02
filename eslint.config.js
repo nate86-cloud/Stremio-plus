c7b8adf98e7c88331b1083a6fe4f26d7399cb605
nate dev <nathanielnartey86@gmail.com>
2026-09-02 22:57:06 +0000

    fix: eslint ignore build/release

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'stremio-server', 'node_modules']),
+  globalIgnores(['dist/**', 'stremio-server/**', 'node_modules/**', 'release/**', 'build/**']),
   {
     files: ['electron.js', 'preload.cjs'],
     languageOptions: {
